268e0d5c80e6723306bc5f7fc7d999ec9716893f
Konasani Teja Maheswara Reddy <2200080216.aids@gmail.com>
2026-06-18 13:36:48 +0530

    Add src/components/layout/Layout.jsx

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Footer from './Footer'
+import MobileBottomNav from './MobileBottomNav'
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+      <MobileBottomNav />
+    </div>
+  )
+}
